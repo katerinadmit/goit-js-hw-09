73baec5ed0e84e4de462f6e4d8bce6822111e3f0
Katerina Dmytrenko <107727106+katerinadmit@users.noreply.github.com>
2022-11-10 21:50:45 +0200

    first task

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,28 @@
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  }
+  const btnStart = document.querySelector('button[data-start]') 
+const btnStop = document.querySelector('button[data-stop]')
+const bodyElement = document.querySelector('body')
+console.log(btnStart)
+console.log(btnStop)
 
+let timerId = null;
+
+btnStop.disabled = true;
+
+btnStart.addEventListener('click', () => {
+    timerId = setInterval(() => {
+        bodyElement.style.backgroundColor = getRandomHexColor();
+    }, 1000);
+  
+    btnStart.disabled = true;
+    btnStop.disabled = false;
+     });
+
+     btnStop.addEventListener('click', () => {
+        clearInterval(timerId);
+        btnStop.disabled = true;
+        btnStart.disabled = false;
+        
+      });
