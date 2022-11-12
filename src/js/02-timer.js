03db366a38d0d3cd9193c55d6fca82a5d56ff514
Katerina Dmytrenko <107727106+katerinadmit@users.noreply.github.com>
2022-11-12 23:55:59 +0200

    2

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -1,3 +1,22 @@
 import Notiflix from 'notiflix';
 import flatpickr from 'flatpickr';
 import 'flatpickr/dist/flatpickr.min.css';
+
+console.log(flatpickr);
+const refs = {
+  input: document.querySelector(`#datetime-picker`),  
+  days: document.querySelector('[data-days]'),
+  hours: document.querySelector('[data-hours]'),
+  minutes: document.querySelector('[data-minutes]'),
+  seconds: document.querySelector('[data-seconds]'),
+  startBtn: document.querySelector('[data-start]'),
+};
+const options = {
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+  onClose(selectedDates) {
+    console.log(selectedDates[0]);
+  },
+};
